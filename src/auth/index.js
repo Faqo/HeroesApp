797a14a90af09099b2fa88f7b92b3fea82d66276
Frank <faqodfz@gmail.com>
2022-07-12 14:36:34 -0400

    se agrego una separacion entre rutas de heroes y las de usuario

---
+++ src/auth/index.js
@@ -0,0 +1,4 @@
+export * from './pages';
+// export * from './components';
+// export * from './helpers';
+// export * from './hooks';
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,17 @@
-import { Route,Routes, Navigate } from 'react-router-dom';
-import { LoginPage } from '../auth/pages/LoginPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { Navbar } from '../ui';
+import { Route,Routes } from 'react-router-dom';
+
+import { LoginPage } from '../auth';
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
-                
+            
                 <Route path="Login" element={<LoginPage />} />
+                
+                <Route path="/*" element={<HeroesRoutes />} />
 
-                <Route path="/" element={<Navigate to='/marvel' />} />
             </Routes>
         </>
     )
